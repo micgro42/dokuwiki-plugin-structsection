e30471396cd51e8344b02a773497ab486b2f9103
Michael Große <mic.grosse@googlemail.com>
2021-01-03 10:51:49 +0100

    Run CI only on supported version
    Also better annotations for PR

--- action.php
+++ action.php
@@ -17,9 +17,8 @@ final class action_plugin_structsection extends \DokuWiki_Action_Plugin
      * @param \Doku_Event_Handler $controller DokuWiki's event controller object
      * @return void
      */
-    public function register(\Doku_Event_Handler $controller): void
-    {
-        $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, 'addPageRevisionToJSINFO');
+    public function register(\Doku_Event_Handler $controller): void {
+      $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, 'addPageRevisionToJSINFO');
         $controller->register_hook('PARSER_HANDLER_DONE', 'AFTER', $this, 'appendPluginOutputToPage');
         $controller->register_hook('PLUGIN_STRUCT_TYPECLASS_INIT', 'BEFORE', $this, 'registerTypeWithStructPlugin');
     }
