6c649f20e95bea5d0af27d09133c8155f62646fa
Michael Große <mic.grosse@googlemail.com>
2021-01-26 11:12:13 +0100

    Add meaningful test
    If we have dedicated testing infrastructure, we should also have tests.

---
+++ _test/Section.test.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use dokuwiki\plugin\structsection\types\Section;
+
+/**
+ * Tests for the struct Section type
+ *
+ * @group plugin_structsection
+ * @group plugins
+ */
+class SectionTest extends PHPUnit\Framework\TestCase {
+
+    public function test_valueEditorTest() {
+        $section = new Section();
+        $name ='testName';
+        $rawValue='testRawValue';
+        $htmlID='testHtmlId';
+
+        $actual = $section->valueEditor($name, $rawValue, $htmlID);
+
+        self::assertEquals(
+            "<textarea name=\"$name\" class=\"struct_section\" id=\"$htmlID\">$rawValue</textarea>",
+            $actual
+        );
+    }
+
+    // TODO: add test for placeholder in config
+}
